ee3b471ec652f13d521392d350bcfac402141c39
matijagu <matija@amondi-media.com>
2022-02-28 15:25:38 +0100

    Forecast Widget - 01

---
+++ forecast_widget/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Forecast Widget - Customization',
+    'version': '0.1',
+    'author': 'Amondi Media d.o.o.',
+    'website': 'https://amondi-media.com',
+    'description': """
+                    Change color or Forecast Widget. When Available qty is less than 0, change color to red, when Available qty is greater than 0, change color to green, when Available qty is less than 5 and greater than 0, change color to yellow.
+
+    """,
+    'summary': """
+                    Change color of Forecast Widget according to Available qty.
+    """,
+    'depends': [
+        'account', 'sale_stock',
+    ],
+    'data': [
+
+    ],
+    'qweb': [
+        'static/src/xml/forecast_widget.xml',
+    ],
+    'installable': True,
+    'license': 'AGPL-3',
+}
---
+++ forecast_widget/models/forecast_widget.py
@@ -0,0 +1,6 @@
+# from odoo import models, fields, api
+
+# class forecast_widget(models.Model):
+#     _name = 'forecast_widget'
+
+#     test = fields.Char(string="Test")
